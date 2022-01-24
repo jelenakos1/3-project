f0d44096188920c430afb736b66f976814ad3021
jelenakos1 <93394910+jelenakos1@users.noreply.github.com>
2022-01-24 19:44:23 +0200

    *

--- 3-projektas/database/migrations/2022_01_24_165306_create_students_table.php
+++ 3-projektas/database/migrations/2022_01_24_165306_create_students_table.php
@@ -15,6 +15,11 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('surname');
+            $table->bigInteger('group_id');
+            $table->string('image_url');
+
             $table->timestamps();
         });
     }
